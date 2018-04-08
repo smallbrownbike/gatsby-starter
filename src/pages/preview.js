01b81fc2ca4a3a39718d26d605b1a916d796306b
smallbrownbike <ekinseydev@gmail.com>
2018-04-08 15:49:47 -0700

    fix preview

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -5,21 +5,23 @@ import queryString from 'querystring'
 export default class Preview extends React.Component {
   constructor(props) {
     super(props)
-    const entryId = queryString.parse(props.location.search)['?entryId']
-    this.state = { preview: null, entryId }
+    this.state = { preview: null }
   }
 
   componentDidMount() {
+    const entryId = queryString.parse(this.props.location.search)['?entryId']
     fetch(
       `https://preview.contentful.com/spaces/${
         process.env.GATSBY_CONTENTFUL_SPACE_ID
-      }/entries/${this.state.entryId}?access_token=${
+      }/entries/${entryId}?access_token=${
         process.env.GATSBY_CONTENTFUL_PREVIEW_TOKEN
       }`
     )
       .then(res => res.json())
       .then(json =>
-        this.setState({ preview: json.fields ? json.fields.desc : null })
+        this.setState({
+          preview: json.fields ? json.fields.content : null,
+        })
       )
   }
 
